425a83b5b181217122de010979719cd09a5567cb
Victor Ndaba <mwangindaba@gmail.com>
2022-04-14 22:34:27 +0300

    📦 feat: added bg colors escape sequences

--- src/tc_color.h
+++ src/tc_color.h
@@ -21,6 +21,15 @@
 #define TC_B_CYAN "\x1B[0;36m"
 #define TC_B_WHITE "\x1B[0;37m"
 
+#define TC_BG_NORM "\x1B[40m"
+#define TC_BG_RED "\x1B[41m"
+#define TC_BG_GREEN "\x1B[42m"
+#define TC_BG_YELLOW "\x1B[43m"
+#define TC_BG_BLUE "\x1B[44m"
+#define TC_BG_MAGENTA "\x1B[45m"
+#define TC_BG_CYAN "\x1B[46m"
+#define TC_BG_WHITE "\x1B[47m"
+
 void color_display(char *args, char *color)
 {
     printf("%s%s%s", color, args, TC_NORM);
